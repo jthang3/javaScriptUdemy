07eb24fb6dd9951b52dce2173c09e4ad0a872e5a
jthang3 <jthang3@gmail.com>
2021-01-10 22:11:22 -0500

    Add files via upload

--- 01-Fundamental-Part1/final/starter/script.js
+++ 01-Fundamental-Part1/final/starter/script.js
@@ -29,3 +29,31 @@ BigInt (ES2020) - larger integers than the Number type can hold.
 
 JavaScript has dynamic typing: We do not have to manually define the data type of 
 the value stored in a variable. Data types are determined automatically. */
+
+
+//SRTING
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2020;
+
+const jonas = "I'm "+firstName+ ', a '+ (year - birthYear) + " years old teacher!";
+const john = `I'm ${firstName}, a ${year - birthYear} years old teacher!`; 
+console.log(jonas);
+console.log(john);
+
+console.log("String with \n\ multiple\n\ lines");
+
+console.log(`Strings
+with
+Multiple Lines`);
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if(isOldEnough){
+    console.log("Sarah can start driving 👌🤦‍♂️");
+}
+else{
+    console.log(`Sarah is too young. Wait another ${18 - age} Years 😢`);
+}
